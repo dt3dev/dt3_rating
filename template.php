0b3b38b3fb318c4650d4e8cf6854fb6de1dd203f
Bruno Vianna <luizbweb@gmail.com>
2019-10-19 19:54:11 -0300

    Permite personalizar os cirterios de avaliacao

--- template.php
+++ template.php
@@ -5,7 +5,7 @@ $product_post_id = get_the_ID();
 
 // Trate os loops para um grande numero de avaliações
 // Para fins de contagem use o loop com todos os itens
-$loop = new WP_Query( array( 
+$loop = new WP_Query( array(
     'post_type'         => 'dt3-rating',
     'posts_per_page'    => -1,
     'meta_key'          => 'dt3_rating_post_id',
@@ -15,7 +15,7 @@ $loop = new WP_Query( array(
 
 // Para carregamento retorne deste loop apenas as páginas que deseja de 4 em 4 páginas.
 // Carregar inicialmente apenas 4 avaliações
-$partial_loop = new WP_Query( array( 
+$partial_loop = new WP_Query( array(
     'post_type'         => 'dt3-rating',
     'posts_per_page'    => 4,
     'meta_key'          => 'dt3_rating_post_id',
@@ -58,7 +58,7 @@ $partial_loop = new WP_Query( array(
                             <?php
                                 dt3_rating_the_averarge_stars( $stars_average );
                             ?>
-                           
+
                             <img id="arrow-clipboard" src="<?php echo PLUGIN_URL; ?>dt3-rating/images/expand-button.svg" alt="" style="padding: 0 10px">
                         </div>
                         <div class="users-rating-mobile">
@@ -68,7 +68,7 @@ $partial_loop = new WP_Query( array(
                                     $dt3_rating_total = dt3_rating_total( $loop, $product_post_id );
                                     echo esc_html( $dt3_rating_total ) ;
                                 ?>
-                                <?php 
+                                <?php
                                     echo 1 == $dt3_rating_total ? 'avaliação' : 'avaliações';
                                 ?>
                             )</p>
@@ -77,7 +77,7 @@ $partial_loop = new WP_Query( array(
                             <div class="percent-stars">
                                 <p>5</p>
                                 <img src="<?php echo PLUGIN_URL; ?>dt3-rating/images/red-star.svg" alt="">
-                            </div> 
+                            </div>
                             <div class="percent-bar">
                                 <div class="percent-out">
                                     <div class="percent-in-5"></div>
@@ -95,7 +95,7 @@ $partial_loop = new WP_Query( array(
                             <div class="percent-stars">
                                 <p>4</p>
                                 <img src="<?php echo PLUGIN_URL; ?>dt3-rating/images/red-star.svg" alt="">
-                            </div> 
+                            </div>
                             <div class="percent-bar">
                                 <div class="percent-out">
                                     <div class="percent-in-4"></div>
@@ -113,7 +113,7 @@ $partial_loop = new WP_Query( array(
                             <div class="percent-stars">
                                 <p>3</p>
                                 <img src="<?php echo PLUGIN_URL; ?>dt3-rating/images/red-star.svg" alt="">
-                            </div> 
+                            </div>
                             <div class="percent-bar">
                                 <div class="percent-out">
                                     <div class="percent-in-3"></div>
@@ -131,7 +131,7 @@ $partial_loop = new WP_Query( array(
                             <div class="percent-stars">
                                 <p>2</p>
                                 <img src="<?php echo PLUGIN_URL; ?>dt3-rating/images/red-star.svg" alt="">
-                            </div> 
+                            </div>
                             <div class="percent-bar">
                                 <div class="percent-out">
                                     <div class="percent-in-2"></div>
@@ -149,7 +149,7 @@ $partial_loop = new WP_Query( array(
                             <div class="percent-stars">
                                 <p>1</p>
                                 <img src="<?php echo PLUGIN_URL; ?>dt3-rating/images/red-star.svg" alt="">
-                            </div> 
+                            </div>
                             <div class="percent-bar">
                                 <div class="percent-out">
                                     <div class="percent-in-1"></div>
@@ -181,15 +181,19 @@ $partial_loop = new WP_Query( array(
                                 <?php  $recommended = dt3_rating_get_recommendations( $loop, $product_post_id ); ?>
                                 <?php echo esc_html( $recommended ); ?>
                                 clientes
-                            </span> 
+                            </span>
                             recomendariam este produto para um amigo
                         </div>
                     </div>
                 </div>
+                <?php
+                  $options = get_option('dt3_rating_options');
+                ?>
                 <div class="details">
                     <div class="comfort">
-                        <h3>CONFORTO 
+                        <h3>
                         <?PHP
+                            echo $options['conforto'] == '' ? 'Conforto': $options['conforto'];
                             $confort_rate = dt3_rating_attribute_average( $loop, 'dt3_rating_confort' );
                             // echo $confort_rate;
                         ?>
@@ -200,13 +204,14 @@ $partial_loop = new WP_Query( array(
                             ?>
                         </div>
                         <div class="rate-text">
-                            <p>POUCO<br/>CONFORTÁVEL</p>
-                            <p style="text-align: end">MUITO<br/>CONFORTÁVEL</p>
+                            <p>1</p>
+                            <p style="text-align: end">5</p>
                         </div>
                     </div>
                     <div class="quality">
-                        <h3>QUALIDADE
+                        <h3>
                         <?PHP
+                            echo $options['qualidade'] == '' ? 'Qualidade': $options['qualidade'];
                             $quality_rate = dt3_rating_attribute_average( $loop, 'dt3_rating_quality' );
                             // echo $quality_rate;
                         ?>
@@ -217,13 +222,14 @@ $partial_loop = new WP_Query( array(
                             ?>
                         </div>
                         <div class="rate-text">
-                            <p>BÁSICA</p>
-                            <p style="text-align: end">EXCEPCIONAL</p>
+                            <p>1</p>
+                            <p style="text-align: end">5</p>
                         </div>
                     </div>
                     <div class="features">
-                        <h3>CARACTERÍSTICAS
+                        <h3>
                         <?PHP
+                            echo $options['caracteristicas'] == '' ? 'Caracteristicas': $options['caracteristicas'];
                             $features_rate = dt3_rating_attribute_average( $loop, 'dt3_rating_features' );
                             // echo $features_rate;
                         ?>
@@ -234,8 +240,8 @@ $partial_loop = new WP_Query( array(
                             ?>
                         </div>
                         <div class="rate-text">
-                            <p>FRACO</p>
-                            <p style="text-align: end">EXCEPCIONAL</p>
+                            <p>1</p>
+                            <p style="text-align: end">5</p>
                         </div>
                     </div>
                 </div>
@@ -273,11 +279,11 @@ $partial_loop = new WP_Query( array(
                 <div id="rating_title" class="title-input">
                     <span class="title-span hide-tooltip">Insira o titulo da avaliação<span></span></span>
                     <label for="title">Título da avaliação <span class="add-color-asterisc">*</span></label>
-                    <input class="rating-title" type="text" name="title" placeholder="Exemplo: A cadeira mais confortável do mercado" required maxlength="100">
+                    <input class="rating-title" type="text" name="title" placeholder="Exemplo: O melhor produto do mercado" required maxlength="100">
                 </div>
                 <div class="positive-input">
                     <label for="positive">Pontos positivos</label>
-                    <textarea class="rating-positive" id="positive-input" type="text" name="positive" placeholder="O que você gostou na DT3?" rows="4" maxlength="300"></textarea>
+                    <textarea class="rating-positive" id="positive-input" type="text" name="positive" placeholder="O que você gostou?" rows="4" maxlength="300"></textarea>
                     <p style="text-align: end; color: #333; padding: 5px 0 0 0"><span class="positive-value">-300</span> caracteres</p>
                 </div>
                 <div class="improvement-input">
@@ -302,121 +308,133 @@ $partial_loop = new WP_Query( array(
                       </span>
                 </div>
                 <div class="comfort-form">
-                    <h3>Conforto</h3>
+                    <h3>
+                        <?php
+                            echo $options['conforto'] == '' ? 'Conforto': $options['conforto'];
+                        ?>
+                    </h3>
                     <div class="comfort-radios">
                         <div class="comfort-radios-item">
                             <label for="comfort-1" class="container">
                                 <input class="rating-confort" type="radio" name="confort-radio" value="1" id="comfort-1">
                                 <span class="checkmark"></span>
-                                Pouco confortável
+                                1
                             </label>
                         </div>
                         <div class="comfort-radios-item">
                             <label for="comfort-2" class="container">
                                 <input class="rating-confort" type="radio" name="confort-radio" value="2" id="comfort-2">
                                 <span class="checkmark"></span>
-                                Incômodo
+                                2
                             </label>
                         </div>
                         <div class="comfort-radios-item">
                             <label for="comfort-3" class="container">
                                 <input class="rating-confort" type="radio" name="confort-radio" value="3" id="comfort-3">
                                 <span class="checkmark"></span>
-                                Razoável
+                                3
                             </label>
                         </div>
                         <div class="comfort-radios-item">
                             <label for="comfort-4" class="container">
                                 <input class="rating-confort" type="radio" name="confort-radio" value="4" id="comfort-4">
                                 <span class="checkmark"></span>
-                                Confortável
+                                4
                             </label>
                         </div>
                         <div class="comfort-radios-item">
                             <label for="comfort-5" class="container">
                                 <input class="rating-confort" type="radio" name="confort-radio" value="5" id="comfort-5">
                                 <span class="checkmark"></span>
-                                Muito confortável
+                                5
                             </label>
                         </div>
                     </div>
                 </div>
                 <div class="quality-form">
-                    <h3>Qualidade</h3>
+                    <h3>
+                      <?php
+                          echo $options['qualidade'] == '' ? 'Qualidade': $options['qualidade'];
+                      ?>
+                    </h3>
                     <div class="quality-radios">
                         <div class="quality-radios-item">
                             <label for="quality-1" class="container">
                                 <input class="rating-quality" type="radio" name="quality-radio" value="1" id="quality-1">
                                 <span class="checkmark"></span>
-                                Básica
+                                1
                             </label>
                         </div>
                         <div class="quality-radios-item">
                             <label for="quality-2" class="container">
                                 <input class="rating-quality" type="radio" name="quality-radio" value="2" id="quality-2">
                                 <span class="checkmark"></span>
-                                Insatisfatória
+                                2
                             </label>
                         </div>
                         <div class="quality-radios-item">
                             <label for="quality-3" class="container">
                                 <input class="rating-quality" type="radio" name="quality-radio" value="3" id="quality-3">
                                 <span class="checkmark"></span>
-                                Razoável
+                                3
                             </label>
                         </div>
                         <div class="quality-radios-item">
                             <label for="quality-4" class="container">
                                 <input class="rating-quality" type="radio" name="quality-radio" value="4" id="quality-4">
                                 <span class="checkmark"></span>
-                                Boa
+                                4
                             </label>
                         </div>
                         <div class="quality-radios-item">
                             <label for="quality-5" class="container">
                                 <input class="rating-quality" type="radio" name="quality-radio" value="5" id="quality-5">
                                 <span class="checkmark"></span>
-                                Excepcional
+                                5
                             </label>
                         </div>
                     </div>
                 </div>
                 <div class="features-form">
-                    <h3>Características</h3>
+                    <h3>
+                      <?php
+                          echo $options['caracteristicas'] == '' ? 'Caracteristicas': $options['caracteristicas'];
+                      ?>
+                    </h3>
                     <div class="features-radios">
                         <div class="features-radios-item">
                             <label for="features-1" class="container">
                                 <input class="rating-features" type="radio" name="features-radio" value="1" id="features-1">
                                 <span class="checkmark"></span>
-                                Fraco
+                                1
                             </label>
                         </div>
                         <div class="features-radios-item">
                             <label for="features-2" class="container">
                                 <input class="rating-features" type="radio" name="features-radio" value="2" id="features-2">
                                 <span class="checkmark"></span>
-                                Insatisfatório
+                                2
                             </label>
                         </div>
                         <div class="features-radios-item">
                             <label for="features-3" class="container">
                                 <input class="rating-features" type="radio" name="features-radio" value="3" id="features-3">
                                 <span class="checkmark"></span>
-                                Razoável
+                                3
                             </label>
                         </div>
                         <div class="features-radios-item">
                             <label for="features-4" class="container">
                                 <input class="rating-features" type="radio" name="features-radio" value="4" id="features-4">
                                 <span class="checkmark"></span>
-                                Bom
+                                4
                             </label>
                         </div>
                         <div class="features-radios-item">
                             <label for="features-5" class="container">
                                 <input class="rating-features" type="radio" name="features-radio" value="5" id="features-5">
                                 <span class="checkmark"></span>
-                                Excepcional
+                                5
                             </label>
                         </div>
                     </div>
@@ -459,10 +477,10 @@ $partial_loop = new WP_Query( array(
                 // Retorne um partial loop que será usado apenas na primeira iteração de 4 posts
                 // Para cada novo ciclo execute novos WP_Querys retornando novos dados
                 // Conte cada post e armazene em data-load
-                
+
                 // Number of ratings loaded
                 $ratings_loaded = 0;
-                
+
                 while ( $partial_loop->have_posts() ) : $partial_loop->the_post();
 
                 $rating_post_id = get_the_ID();
@@ -496,7 +514,7 @@ $partial_loop = new WP_Query( array(
                                 <?php
                                     // Exibir os icones de recomendação ou não
                                     // Exibir exibir texto de recomendação
-                                    // $rating_recomendations = dt3_rating_get_field( 'dt3_rating_recomendations' ); 
+                                    // $rating_recomendations = dt3_rating_get_field( 'dt3_rating_recomendations' );
                                     // echo $rating_recomendations;
                                     dt3_rating_the_recommendation ();
                                 ?>
@@ -510,7 +528,7 @@ $partial_loop = new WP_Query( array(
 
                 endwhile;
             ?>
-            
+
         </section>
         <section class="rating-more">
             <div class="rating-loading"></div>
